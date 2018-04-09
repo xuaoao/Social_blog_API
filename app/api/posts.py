b1a65f95f4284630f13c83e4decd5c031bdd6985
xuaoao <di94sh@hotmail.com>
2018-04-09 19:23:43 +0800

    v0.1

--- app/api/posts.py
+++ app/api/posts.py
@@ -1,57 +1,73 @@
-from flask import jsonify, request, g, url_for, current_app
+from flask import request, g, url_for, current_app
+from flask_restful import Resource, reqparse
 from .. import db
 from ..models import Post, Permission
-from . import api
+from . import restful
 from .decorators import permission_required
 from .errors import forbidden
 
 
-@api.route('/posts/')
-def get_posts():
-    page = request.args.get('page', 1, type=int)
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
-        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
-        error_out=False)
-    posts = pagination.items
-    prev = None
-    if pagination.has_prev:
-        prev = url_for('api.get_posts', page=page-1)
-    next = None
-    if pagination.has_next:
-        next = url_for('api.get_posts', page=page+1)
-    return jsonify({
-        'posts': [post.to_json() for post in posts],
-        'prev': prev,
-        'next': next,
-        'count': pagination.total
-    })
+class GetPosts(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('page', default=1, type=int, location='args')
+        self.parser.add_argument('title', type=str, location='json')
+        self.parser.add_argument('body', type=str, location='json')
+        super().__init__()
+
+    def get(self):
+        page = self.parser.parse_args().get('page')
+        pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+            page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+            error_out=False)
+        posts = pagination.items
+        prev = None
+        if pagination.has_prev:
+            prev = url_for('api.get_posts', page=page - 1)
+        next = None
+        if pagination.has_next:
+            next = url_for('api.get_posts', page=page + 1)
+        return {
+            'posts': [post.to_json() for post in posts],
+            'prev': prev,
+            'next': next,
+            'count': pagination.total
+        }
+
+    @permission_required(Permission.WRITE)
+    def post(self):
+        args = self.parser.parse_args()
+        post = Post.from_json(args)
+        post.author = g.current_user
+        db.session.add(post)
+        db.session.commit()
+        return post.to_json(), 201,\
+               {'Location': url_for('api.get_post', id=post.id)}
 
 
-@api.route('/posts/<int:id>')
-def get_post(id):
-    post = Post.query.get_or_404(id)
-    return jsonify(post.to_json())
+class GetPost(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('body', type=str, location='json')
+        super().__init__()
+
+    def get(self, id):
+        post = Post.query.get_or_404(id)
+        return post.to_json()
+
+    @permission_required(Permission.WRITE)
+    def put(self, id):
+        args = self.parser.parse_args()
+        post = Post.query.get_or_404(id)
+        if g.current_user != post.author and \
+                not g.current_user.can(Permission.ADMIN):
+            return forbidden('Insufficient permissions')
+        post.body = args.get('body', post.body)
+        db.session.add(post)
+        db.session.commit()
+        return post.to_json()
 
 
-@api.route('/posts/', methods=['POST'])
-@permission_required(Permission.WRITE)
-def new_post():
-    post = Post.from_json(request.json)
-    post.author = g.current_user
-    db.session.add(post)
-    db.session.commit()
-    return jsonify(post.to_json()), 201, \
-        {'Location': url_for('api.get_post', id=post.id)}
+restful.add_resource(GetPosts, '/posts/', endpoint='get_posts')
+restful.add_resource(GetPost, '/posts/<int:id>', endpoint='get_post')
 
-
-@api.route('/posts/<int:id>', methods=['PUT'])
-@permission_required(Permission.WRITE)
-def edit_post(id):
-    post = Post.query.get_or_404(id)
-    if g.current_user != post.author and \
-            not g.current_user.can(Permission.ADMIN):
-        return forbidden('Insufficient permissions')
-    post.body = request.json.get('body', post.body)
-    db.session.add(post)
-    db.session.commit()
-    return jsonify(post.to_json())
